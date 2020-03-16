43a7c3a7c89fd4dde668af07bdc81bec87bd11ef
Monika Leszczynska <leszczynska.monikaa@gmail.com>
2020-03-16 15:43:22 +0100

    feat: JS actions

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,16 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const message = document.querySelector('.form__message--js');
+const load = document.querySelector('.form__button--load--js');
+const save = document.querySelector('.form__button--save--js');
 
-console.log(`Hello world!`);
+save.addEventListener('click', (e) =>{
+  e.preventDefault();
+  localStorage.setItem('message', message.value);
+})
 
-
+load.addEventListener('click', (e) =>{
+  e.preventDefault();
+  message.value = localStorage.getItem('message');
+})
